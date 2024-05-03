8a3bfc2d6d41fa0104b179d5f7d06a2d5d311cfd
karlwoien <karlwoien@gmail.com>
2024-05-03 16:18:55 +0200

    adds handler functionality to create post and edit post

---
+++ src/js/handlers/editPost.mjs
@@ -0,0 +1,33 @@
+import { getPost } from "../api/posts/get.mjs";
+import { updatePost } from "../api/posts/update.mjs"
+
+export async function setEditPostFormListener () {
+    const form = document.querySelector("#edit-post");
+
+    const url = new URL (location.href);
+    const id = url.searchParams.get("id");
+
+    if (form) {
+        const button = form.querySelector("button");
+        button.disabled = true;
+
+        const post = await getPost(id);
+
+        form.title.value = post.title;
+        form.body.value = post.body;
+        form.tags.value = post.tags;
+        form.media.value = post.media;
+
+        button.disabled = false;
+
+        form.addEventListener("submit", (event)=> {
+            event.preventDefault();
+            const form = event.target;
+            const formData = new FormData(form);
+            const post = Object.fromEntries(formData.entries());
+            post.id = id;
+    
+            updatePost(post);
+        });
+    };    
+};
