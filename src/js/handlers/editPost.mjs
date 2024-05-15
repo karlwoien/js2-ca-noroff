c1fbd59babc6095477b386ee210c89c0d8697cd8
karlwoien <karlwoien@gmail.com>
2024-05-15 17:07:16 +0200

    adds update to edit post handler

--- src/js/handlers/editPost.mjs
+++ src/js/handlers/editPost.mjs
@@ -1,17 +1,43 @@
 import { getPost } from "../api/posts/get.mjs";
 import { updatePost } from "../api/posts/update.mjs"
+import { load } from "../storage/storageHandler.mjs";
 
+// Function to check if the author of the post is the logged in user
+async function isUserLoggedIn(postAuthor) {
+    const { name } = await load("profile");
+    return postAuthor === name;
+}
+
+// Function to show or hide the updateDelete section on view post by ID page based on logged-in status
+async function showUpdateDeleteSection(postAuthor) {
+    const updateDeleteSection = document.getElementById("updateDelete");
+    if (await isUserLoggedIn(postAuthor)) {
+        updateDeleteSection.style.display = 'block';
+    } else {
+        updateDeleteSection.style.display = 'none';
+    }
+}
+
+// Function to set up the edit post form with event listeners
 export async function setEditPostFormListener () {
-    const form = document.querySelector("#edit-post");
-
     const url = new URL (location.href);
     const id = url.searchParams.get("id");
+
+    const post = await getPost(id);
+
+    await showUpdateDeleteSection(post.author.name);
+
+    if (!await isUserLoggedIn(post.author.name)) {
+        return;
+    }
+
+    const form = document.querySelector("#edit-post");
 
     if (form) {
         const button = form.querySelector("button");
         button.disabled = true;
 
-        const post = await getPost(id);
+       
 
         form.title.value = post.title;
         form.body.value = post.body;
