4e0a1f24185b4f76cb968f23404344396e43c6b9
karlwoien <karlwoien@gmail.com>
2024-05-15 11:05:16 +0200

    adds title to search function for more search options

--- src/js/handlers/searchPosts.mjs
+++ src/js/handlers/searchPosts.mjs
@@ -9,10 +9,11 @@ export async function setSearchPostListener() {
         const searchText = searchInput.value.toLowerCase();
         const allPosts = await getPosts();
 
-        // Filter posts by matching the search text against post author name or post body content. Also checking for null values before accessing properties
+        // Filter posts by matching the search text against post author name, post body content or post title content. Also checking for null values before accessing properties
         const filteredPosts = allPosts.filter(post =>
             (post.author && post.author.name && post.author.name.toLowerCase().includes(searchText)) ||
-            (post.body && post.body.toLowerCase().includes(searchText))
+            (post.body && post.body.toLowerCase().includes(searchText)) ||
+            (post.title && post.title.toLowerCase().includes(searchText))
         );
 
         feed.innerHTML = "";  // Clear the feed
